f668b42d549a910601c89dc45b4c1a35d9388e41
gigigimay <mo.maytiya_st@tni.ac.th>
2019-12-12 22:37:19 +0700

    add react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import styles from './styles.css'
-import { add, subtract, createObject } from './module'
+import React from 'react'
+import { render } from 'react-dom'
 
-if (module.hot) {
-  module.hot.accept()
-}
+const MainApp = () => (
+  <h1>Hello React!!</h1>
+)
 
-const resultA = add(2, 3)
-const resultB = subtract(5, 1)
-
-console.log(resultA, resultB, createObject({ a: 1 }))
-console.log(styles.localClass) // _19OBmKu4X8SmIISJiYXz8U
-console.log(styles.globalClass) // undefined
-
-console.log(process.env.APP_NAME) // 'reachatty'
+render(<MainApp />, document.getElementById('app'))
