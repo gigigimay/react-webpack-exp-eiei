68f8f26780173520b3ecfb1b67356834f8e1a960
gigigimay <mo.maytiya_st@tni.ac.th>
2019-12-12 23:11:20 +0700

    config eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+/* eslint-disable react/jsx-fragments */
 import React from 'react'
 import { render } from 'react-dom'
 
 if (module.hot) module.hot.accept()
 
+const a = 1
+
 const App = () => (
-  <h1>Hello React!</h1>
+  <React.Fragment>
+    <h1>Hello React!</h1>
+    <p>
+      {a > 2 ? 'true' : 'false'}
+    </p>
+  </React.Fragment>
 )
 
 render(<App />, document.getElementById('app'))
